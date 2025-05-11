a1b7421200a35cb595715369905420fc2ed63ff3
Ron Sloan <xkiller1177@gmail.com>
2025-05-11 12:43:48 -0700

    Update build.gradle.kts

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -84,6 +84,8 @@ desugaring {
 
 dependencies {
   debugImplementation(libs.common.leakcanary)
+implementation("org.bouncycastle:bcprov-jdk18on:1.78")
+    implementation("org.bouncycastle:bcpkix-jdk18on:1.78")
 
   // Annotation processors
   kapt(libs.common.glide.ap)
